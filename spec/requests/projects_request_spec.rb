5ded3b36d616d5f74e45623c1b1647ba5a94d59c
Horace Williams <horace.d.williams@gmail.com>
2012-07-11 16:19:24 -0400

    belay that failing test -- to the feature branch¡

--- spec/requests/projects_request_spec.rb
+++ spec/requests/projects_request_spec.rb
@@ -3,11 +3,6 @@ require 'spec_helper'
 describe "Creating a project" do
 
   context "When I visit the homepage" do
-    before(:each) { visit root_path }
-
-    it "shows a form for creating a new project" do
-      page.should have_selector("#new_project")
-    end
   end
 
 end
