c4e5aa2b13317b353e24463effa0d5ccb775f38b
Horace Williams <horace.d.williams@gmail.com>
2012-07-11 16:18:20 -0400

    guard to the rescue

---
+++ spec/requests/projects_request_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "Creating a project" do
+
+  context "When I visit the homepage" do
+    before(:each) { visit root_path }
+
+    it "shows a form for creating a new project" do
+      page.should have_selector("#new_project")
+    end
+  end
+
+end
+
