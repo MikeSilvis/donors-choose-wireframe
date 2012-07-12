1f38124b0f7553ef07fb34640d96be94867cc72b
Darrell Rivera <darrell.rivera@livingsocial.com>
2012-07-12 14:16:24 -0400

    Challenge only belongs to one project

--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -1,13 +1,14 @@
 class ChallengesController < ApplicationController
   def new
-    @challenge = Challenge.new(params[:challenge], :project_id => params[:project_id])
+    @challenge = Challenge.new(params[:challenge])
+    @project_id = params[:project_id]
   end
 
   def create
-    raise params[:project_id].inspect
     @challenge = Challenge.new(params[:challenge])
     if @challenge.save
-      redirect_to root_path
+      # TODO: is this n+1?
+      redirect_to project_path(@challenge.project_id)
       flash[:notice] = "Your challenge has been created"
     else
       render :new
