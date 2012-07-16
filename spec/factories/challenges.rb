66eeece7a9c977882996d0edd6be19e0ba1683c1
Darrell Rivera <darrell.rivera@livingsocial.com>
2012-07-16 18:54:05 -0400

    Merging with master

--- spec/factories/challenges.rb
+++ spec/factories/challenges.rb
@@ -1,14 +1,8 @@
 FactoryGirl.define do
   factory :challenge do
-<<<<<<< HEAD
-  	name_of_challenger "Darrell"
-  	title_of_challenge "I will shave off my head"
-  	amount 5
-=======
     sequence(:name_of_challenger) { |n| "Challenger ##{n}" }
     sequence(:title_of_challenge) { |n| "Challenge ##{n}" }
     amount 30
     project_id 1
->>>>>>> 90675ae0a23f0a05d994168e445b575e0aa43d0e
   end
 end
