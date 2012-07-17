86496db6eaaefa752a2cb9c8ec54fedd0cf4db95
michael verdi <michael.v.verdi@gmail.com>
2012-07-17 13:59:23 -0400

    fixed failing tests

--- spec/factories/challenges.rb
+++ spec/factories/challenges.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
     sequence(:title_of_challenge) { |n| "Challenge ##{n}" }
     amount 30
     project_id 1
+    display_media "http://www.flushdoggy.com/wordpress/wp-content/uploads/2012/05/male-dog.jpg"
   end
 end
