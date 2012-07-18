e75366012cfb45e3f23e5fc65493f3b5d7861aa6
Horace Williams <horace.d.williams@gmail.com>
2012-07-18 09:59:43 -0400

    removing helper specs and cleaning up pending tests

--- spec/models/challenge_observer_spec.rb
+++ spec/models/challenge_observer_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe ChallengeObserver do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "#after_create" do
+    before { Challenge.any_instance.stub(:amount_versus_donors_choose_fund) }
+    let(:project) { FactoryGirl.create(:project) }
+
+    it "creates an event for the evidence" do
+      challenge = FactoryGirl.create(:challenge, project: project)
+      Event.last.event_type.should == "challenge_created"
+    end
+  end
 end
