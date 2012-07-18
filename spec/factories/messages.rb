07151b2ac41231fed66fe58e6f3e876d5780a485
michael verdi <michael.v.verdi@gmail.com>
2012-07-18 12:50:20 -0400

    added tests changed text link to link

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,6 +2,19 @@ FactoryGirl.define do
   factory :message do
     sequence(:name) { |n| "My Name #{n}" }
     sequence(:body) { |n| "Message Number #{n}" }
-    sequence(:project_id) { |n| n }
+    project_id 1
+  end
+
+  factory :video_message, parent: :message do
+    body "http://www.youtube.com/watch?v=LiyQ8bvLzIE&feature=g-all-u"
+  end
+
+  factory :image_message, parent: :message do
+    body "http://images4.fanpop.com/image/photos/15800000/Cute-Puppy-puppies-15813366-1600-1200.jpg"
+  end
+
+  factory :link_message, parent: :message do
+    body "http://google.com"
   end
 end
+
