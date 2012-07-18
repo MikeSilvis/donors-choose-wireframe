10b2736b1a4ec7d05a82c04ba16cf79727af54a8
michael verdi <michael.v.verdi@gmail.com>
2012-07-18 15:03:16 -0400

    added doc using yard

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'redis-store'
 gem 'resque', "~> 1.20.0", :require => 'resque/server'
 gem 'resque-scheduler', :require => 'resque_scheduler'
 gem 'embedly'
+gem 'yard'
+gem 'redcarpet'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,4 +1,8 @@
 module ProjectsHelper
+  #Properly displays the message based on its media_type
+  #
+  # @param [Message]
+  # @return [String] the html representation of a message
   def format_message(message)
     unless message.nil?
       case message.media_type
@@ -14,6 +18,11 @@ module ProjectsHelper
     end
   end
 
+  #Returns the html safe markup for the video_html attribute if it exists.
+  #If no attribute exist, it triggers a call to embedly to fetch markup
+  #
+  # @param [Message]
+  # @return [String] the html markup for a message with a video media_type
   def get_video(message)
     if message.video_html
       message.video_html.html_safe
