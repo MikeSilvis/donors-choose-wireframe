ca18d3496dd8ce6dda8662b6d6a725b25a94f9bd
michael verdi <michael.v.verdi@gmail.com>
2012-07-18 09:59:09 -0400

    videos not displaying, but trying to use redis

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,14 +1,16 @@
 module ProjectsHelper
   def format_message(message)
-    case message.media_type
-    when "video"
-      message.embed
-    when "text"
-      link_to(message.url, message.url, target: "_blank")
-    when "image"
-      image_tag(message.url)
-    else
-      message.body
+    unless message.nil?
+      case message.media_type
+      when "video"
+        REDIS.get "message_id_#{message.id}" || message.embed
+      when "text"
+        link_to(message.url, message.url, target: "_blank")
+      when "image"
+        image_tag(message.url)
+      else
+        message.body
+      end
     end
   end
 end
