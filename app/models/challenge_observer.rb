fccb4c1cc70fd943376da1e920df216bc942d649
Darrell Rivera <darrell.rivera@livingsocial.com>
2012-07-19 11:10:31 -0400

    adding back observer around create

--- app/models/challenge_observer.rb
+++ app/models/challenge_observer.rb
@@ -3,10 +3,10 @@ class ChallengeObserver < ActiveRecord::Observer
     Event.create_for(challenge.project_id, challenge, :created)
   end
 
-  # def around_save(challenge)
-  #   if challenge.changed_attributes.keys.include?("met") && challenge.met
-  #     Event.create_for(challenge.project_id, challenge, :met)
-  #   end
-  #   yield
-  # end
+  def around_save(challenge)
+    if challenge.changed_attributes.keys.include?("met") && challenge.met
+      Event.create_for(challenge.project_id, challenge, :met)
+    end
+    yield
+  end
 end
