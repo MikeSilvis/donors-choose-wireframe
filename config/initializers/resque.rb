5a63e621bd2f7500753f4f50bd39b963165a7045
Darrell Rivera <darrell.rivera@livingsocial.com>
2012-07-19 10:54:24 -0400

    fixing errors

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,10 +1,15 @@
-require 'resque'
 if Rails.env.production?
-  redis_url = ENV["REDISTOGO_URL"]
+  require 'resque'
+  redis_url = "redis://redistogo:1d5fe284ccd1c00cf260297db7b6447c@chubb.redistogo.com:9365/"
   uri = URI.parse(redis_url)
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
-else
-  REDIS = Redis.new
+  Resque.redis = REDIS
+  Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
+elsif Rails.env.development?
+  require 'resque'
+  redis_url = 'localhost:6379'
+  uri = URI.parse(redis_url)
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  Resque.redis = REDIS
+  Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
 end
-Resque.redis = REDIS
-Resque.after_fork = Proc.new { ActiveRecord::Base.establish_connection }
