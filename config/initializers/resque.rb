be0c98cdfe13bf17b37c0c7cd5eed4410d1b7dec
Darrell Rivera <darrell.rivera@livingsocial.com>
2012-07-19 09:43:56 -0400

    Add redis2go url to resque initilaizer

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   require 'resque'
-  redis_url = "redis://redistogo:246cb2c3fb7c4a3bfbf854dcab5f5800@cod.redistogo.com:10039/"
+  redis_url = "redis://redistogo:1d5fe284ccd1c00cf260297db7b6447c@chubb.redistogo.com:9365/"
   uri = URI.parse(redis_url)
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Resque.redis = REDIS
