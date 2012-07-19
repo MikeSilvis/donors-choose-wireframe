bca4a274fee81d25787159477fbfd8c08ce73243
michael verdi <michael.v.verdi@gmail.com>
2012-07-19 11:44:12 -0400

    fixing initializer for redis

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   require 'resque'
-  redis_url = "redis://redistogo:1d5fe284ccd1c00cf260297db7b6447c@chubb.redistogo.com:9365/"
+  redis_url = "redis://redistogo:977b137d16dedfbf0a1920b53f6788fe@chubb.redistogo.com:9365/"
   uri = URI.parse(redis_url)
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Resque.redis = REDIS
