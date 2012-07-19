48836019cb5fb1662a8a32073ce623326de940e4
Horace Williams <horace.d.williams@gmail.com>
2012-07-19 10:20:11 -0400

    killing an incorrect column name that got into one of the message migrations

--- db/migrate/20120717221031_remove_columns_from_messages.rb
+++ db/migrate/20120717221031_remove_columns_from_messages.rb
@@ -1,7 +1,6 @@
 class RemoveColumnsFromMessages < ActiveRecord::Migration
   def change
     remove_column :messages, :video_url
-    remove_column :messages, :image_url
     remove_column :messages, :text_url
     add_column :messages, :media_type, :string
     add_column :messages, :url, :string
