45c52698b719f2d3adfecc4551fd48ddbcd1a392
Horace Williams <horace.d.williams@gmail.com>
2012-07-19 09:31:40 -0400

    finally displaying les images¡

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,6 +8,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def default_url
+    "https://image-posts.s3.amazonaws.com/uploads/challenge_evidence/image/4/trout_default.jpeg"
+  end
+
   version :thumb do
     process :resize_to_limit => [200, 200]
   end
