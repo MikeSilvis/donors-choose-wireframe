d0a7161aebda784e86a58d551ba8a6932bcbbab8
Darrell Rivera <darrell.rivera@livingsocial.com>
2012-07-19 13:34:45 -0400

    Chaning redis url

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   require 'resque'
-  redis_url = "redis://redistogo:1d5fe284ccd1c00cf260297db7b6447c@chubb.redistogo.com:9365/"
+  redis_url = "redis://redistogo:1a997ec7ce05b6dcafcd7929bbd46c9e@chubb.redistogo.com:9405"
   uri = URI.parse(redis_url)
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Resque.redis = REDIS
