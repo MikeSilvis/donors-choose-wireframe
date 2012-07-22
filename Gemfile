85b03aa60268d5f65a4e5e3206e6027ee783c9a7
michael verdi <michael.v.verdi@gmail.com>
2012-07-22 11:12:28 -0400

    made gem and updated messages to use it

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'fog'
 gem 'redis-store'
 gem 'resque', "~> 1.20.0", :require => 'resque/server'
 gem 'resque-scheduler', :require => 'resque_scheduler'
-gem 'embedly'
+gem 'text_to_media', github: "verdi327/text_to_media"
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120718203349) do
+ActiveRecord::Schema.define(:version => 20120720195932) do
 
   create_table "challenge_evidences", :force => true do |t|
     t.integer  "challenge_id"
@@ -45,11 +45,9 @@ ActiveRecord::Schema.define(:version => 20120718203349) do
     t.string   "name"
     t.string   "body"
     t.integer  "project_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-    t.string   "media_type"
-    t.string   "url"
-    t.string   "video_html"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.string   "display_html"
   end
 
   create_table "projects", :force => true do |t|
