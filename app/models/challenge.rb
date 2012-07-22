92cdec4821fff0a787741507e68b9f673a2bcb4e
Horace Williams <horace.d.williams@gmail.com>
2012-07-22 16:31:25 -0400

    adding monetized target_funding_cents to challenges to calculate how much money is needed to complete

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,6 +2,8 @@ class Challenge < ActiveRecord::Base
   attr_accessible :name, :title, :amount,
                   :project_id, :display_media
 
+  monetize :target_funding_cents, allow_nil: true
+
   belongs_to :project
   has_many :challenge_evidences
 
@@ -9,14 +11,15 @@ class Challenge < ActiveRecord::Base
   validates_numericality_of :amount, :greater_than => 0
   validate                  :amount_versus_donors_choose_fund, :on => :create
 
-  after_create :create_challenge_creation_event
-
-  def create_challenge_creation_event
-  end
+  after_create :calculate_target_funding
 
   def mark_as_met
     self.met = true
     save
+  end
+
+  def cents_to_complete
+    project.cents_to_complete - target_funding.cents
   end
 
   private
@@ -25,5 +28,14 @@ class Challenge < ActiveRecord::Base
     if self.amount && self.project.cost_to_complete.to_i < self.amount
       errors.add(:amount, "Amount must be less than the cost to complete")
     end
+  end
+
+  def cents_amount
+    amount * 100
+  end
+
+  def calculate_target_funding
+    self.target_funding_cents = (project.cents_to_complete - cents_amount)
+    save
   end
 end
