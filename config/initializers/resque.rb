c0ea83bd9a10330dc8331c1887cb674f265fbf92
Darrell Rivera <darrell.rivera@livingsocial.com>
2012-07-23 13:41:16 -0400

    Adding environment variable for redis server

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
 require 'resque'
 if Rails.env.production?
-  redis_url = "redis://redistogo:1a997ec7ce05b6dcafcd7929bbd46c9e@chubb.redistogo.com:9405"
+  redis_url = ENV["REDISTOGO_URL"]
   uri = URI.parse(redis_url)
   REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 else
