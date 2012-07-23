40861aa75e29a7ed24644a70c38262a5a24ff23a
michael verdi <michael.v.verdi@gmail.com>
2012-07-23 15:32:48 -0400

    the messages are working with ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$("#new_message").live("ajax:complete", function(event,xhr,status){
+  $('#message_name').val('');
+  $('#message_body').val('');
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,14 @@
 class MessagesController < ApplicationController
   def create
     project = Project.find(params[:project_id])
-    message = project.messages.new(params[:message])
-    message.save
-    redirect_to project_path(project)
+    @message = project.messages.new(params[:message])
+    respond_to do |format|
+      if @message.save
+        format.js
+        format.html { redirect_to project_path(project) }
+      else
+        format.html { render action: "new"}
+      end
+    end
   end
 end
