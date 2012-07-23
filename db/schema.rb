862dc4ca4cbb920ab103f298bcf8ef1fc12b90eb
Horace Williams <horace.d.williams@gmail.com>
2012-07-23 12:53:34 -0400

    adding monetized amount_raised_cents for projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120723134325) do
+ActiveRecord::Schema.define(:version => 20120723164514) do
 
   create_table "challenge_evidences", :force => true do |t|
     t.integer  "challenge_id"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(:version => 20120723134325) do
     t.datetime "created_at",          :null => false
     t.datetime "updated_at",          :null => false
     t.string   "donors_choose_id"
+    t.integer  "amount_raised_cents"
   end
 
   create_table "users", :force => true do |t|
