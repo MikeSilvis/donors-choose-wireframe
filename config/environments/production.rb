1b234ec257b1e5c3b1fd4084fe1f1a00b93936b5
Darrell Rivera <darrell.rivera@livingsocial.com>
2012-07-24 00:03:30 -0400

    Fixing gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,6 @@ DonorsChooseWireframe::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
-  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]
 
   # Generate digests for assets URLs
   config.assets.digest = true
